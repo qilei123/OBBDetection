b4248f9b93d4721e6fc874e65739b8f937ede585
qilei123 <qileimail123@gmail.com>
2021-11-09 17:25:42 -0500

    dev

--- mmdet/models/detectors/base.py
+++ mmdet/models/detectors/base.py
@@ -144,7 +144,7 @@ class BaseDetector(nn.Module, metaclass=ABCMeta):
         assert samples_per_gpu == 1
         print(num_augs)
         print(img_metas)
-        print(hasattr(img_metas,'data'))
+        print(hasattr(img_metas[0],'data'))
         if num_augs == 1:
             """
             proposals (List[List[Tensor]]): the outer list indicates test-time
