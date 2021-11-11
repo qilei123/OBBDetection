6e6d47d87312284197b990990d75b6e004b0f5db
qilei123 <qileimail123@gmail.com>
2021-11-11 03:07:15 -0500

    dev

--- mmdet/datasets/obb/trans_drone.py
+++ mmdet/datasets/obb/trans_drone.py
@@ -45,6 +45,8 @@ class TDDataset(CustomDataset):
                 for label in data_info['ann']['labels']:
                     label_counts[int(label)-1]+=1
             print(label_counts)
+            self.cat_ids = [1,2,3]
+            self.cat2label = {1:0,2:1,3:2}
             return data_infos
 
         self.coco = COCO(ann_file)
