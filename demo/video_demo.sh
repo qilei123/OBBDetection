cc59776e45d701718a8ff57f507ec0779ac8ed9b
qilei123 <qileimail123@gmail.com>
2021-11-11 15:21:44 -0500

    dev

--- demo/video_demo.sh
+++ demo/video_demo.sh
@@ -10,4 +10,4 @@ python demo/video_demo.py \
     data/vd/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_vd/latest.pth \
     --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
     --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
-    --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_2.MOV
+    --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_2.MOV --mix
