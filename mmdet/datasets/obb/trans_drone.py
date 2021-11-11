4f2f4c69e5301e352661ac79d1fd24980d7f86dc
qilei123 <qileimail123@gmail.com>
2021-11-11 03:27:35 -0500

    dev

--- mmdet/datasets/obb/trans_drone.py
+++ mmdet/datasets/obb/trans_drone.py
@@ -2,7 +2,7 @@ import itertools
 import logging
 import os.path as osp
 import tempfile
-
+import random
 import mmcv
 import numpy as np
 from mmcv.utils import print_log
@@ -65,7 +65,7 @@ class TDDataset(CustomDataset):
             ann_info = self.coco.load_anns(ann_ids)
             content = self._parse_ann_info(info,ann_info)
             contents.append(content)
-        return contents
+        return random.shuffle(contents)
     def get_cat_ids(self, idx):
         """Get COCO category ids by index.
 
