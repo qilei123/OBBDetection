bbd5f50a95bea635144d21d4591f3d2c0e1a5c08
qilei123 <qileimail123@gmail.com>
2021-11-11 02:39:01 -0500

    dev

--- configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch.py
+++ configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch.py
@@ -66,7 +66,7 @@ test_cfg = dict(
     max_per_img=200)
 # optimizer
 optimizer = dict(
-    lr=0.0025, paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
+    lr=0.005, paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
 optimizer_config = dict(
     _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
 
@@ -106,7 +106,7 @@ test_pipeline = [
 # does evaluation while training
 # uncomments it  when you need evaluate every epoch
 data = dict(
-    samples_per_gpu=4,
+    samples_per_gpu=8,
     workers_per_gpu=8,
     train=dict(
         ann_file=data_root + 'split_set_train/annfiles/patch_annfile.pkl',
