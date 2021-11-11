dc2ff4eafb5521bfbb8fcac22bfb8115c5856625
qilei123 <qileimail123@gmail.com>
2021-11-11 17:36:38 -0500

    dev

--- demo/video_demo.sh
+++ demo/video_demo.sh
@@ -16,6 +16,6 @@ export CUDA_VISIBLE_DEVICES=1
 python demo/video_demo.py \
     configs/obb/app/vis_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_vdr.py \
     data/vdr/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_vdr/latest.pth \
-    \ #--split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
     --video_dir "/home/qilei/DATASETS/trans_drone/trans_drone_videos/DJI_0004 Thermal 400.MOV" \
     --out_dir "/home/qilei/DATASETS/trans_drone/DJI_0004 Thermal 400.MOV" #--mix
+    #--split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
