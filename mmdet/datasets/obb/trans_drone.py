0be107681149e465ef3271322340dae1cead3002
qilei123 <qileimail123@gmail.com>
2021-11-11 03:21:44 -0500

    dev

--- mmdet/datasets/obb/trans_drone.py
+++ mmdet/datasets/obb/trans_drone.py
@@ -41,9 +41,10 @@ class TDDataset(CustomDataset):
             else:
                 data_infos = contents
             label_counts = [0,0,0]
-            for data_info in data_infos:
+            for data_info in data_infos[:1000]:
                 for label in data_info['ann']['labels']:
                     label_counts[int(label)-1]+=1
+            print(label_counts)
             self.cat_ids = [1,2,3]
             self.cat2label = {1:0,2:1,3:2}
             return data_infos[:1000]
