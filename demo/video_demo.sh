0509afd2b57ea64945e24f688cb5e4cdda6ad425
qilei123 <qileimail123@gmail.com>
2021-11-15 17:24:47 -0500

    dev

--- demo/video_demo.sh
+++ demo/video_demo.sh
@@ -10,7 +10,7 @@ python demo/video_demo.py \
     data/vd/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_vd/latest.pth \
     --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
     --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
-    --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_vd.MOV --mix
+    --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_vd.MOV --mix --save_imgs
 
 # export CUDA_VISIBLE_DEVICES=1
 # python demo/video_demo.py \
