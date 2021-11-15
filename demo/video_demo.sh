bf04fb689d505e114f4124bfd73b8b3537e66e3c
qilei123 <qileimail123@gmail.com>
2021-11-14 22:56:01 -0500

    dev

--- demo/video_demo.sh
+++ demo/video_demo.sh
@@ -20,17 +20,17 @@
 #     --out_dir "/home/qilei/DATASETS/trans_drone/DJI_0004 Thermal 400.MOV" #--mix
 #     #--split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
 
-python demo/video_demo.py \
-    configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_mixpatch.py \
-    data/td/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_td_mixpatch/latest.pth \
-    --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
-    --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
-    --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_mixpatch.MOV --mix --save_imgs
-
-# export CUDA_VISIBLE_DEVICES=1
 # python demo/video_demo.py \
-#     configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_mixmorepatch.py \
-#     data/td/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_td_mixmorepatch/latest.pth \
+#     configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_mixpatch.py \
+#     data/td/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_td_mixpatch/latest.pth \
 #     --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
 #     --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
-#     --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_mixmorepatch.MOV --mix --save_imgs
+#     --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_mixpatch.MOV --mix --save_imgs
+
+export CUDA_VISIBLE_DEVICES=1
+python demo/video_demo.py \
+    configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_mixmorepatch.py \
+    data/td/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_td_mixmorepatch/latest.pth \
+    --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
+    --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
+    --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_mixmorepatch.MOV --mix --save_imgs
