dc6c6921e12e139259764f306fa7e1cdf39c77a9
qilei123 <qileimail123@gmail.com>
2021-11-15 17:40:10 -0500

    dev

--- demo/video_demo.sh
+++ demo/video_demo.sh
@@ -5,12 +5,12 @@
 #     --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
 #     --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_1.MOV --mix
 
-python demo/video_demo.py \
-    configs/obb/app/vis_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_vd.py \
-    data/vd/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_vd/latest.pth \
-    --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
-    --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
-    --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_vd.MOV --mix --save_imgs
+# python demo/video_demo.py \
+#     configs/obb/app/vis_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_vd.py \
+#     data/vd/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_vd/latest.pth \
+#     --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
+#     --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
+#     --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_vd.MOV --mix --save_imgs
 
 # export CUDA_VISIBLE_DEVICES=1
 # python demo/video_demo.py \
@@ -35,10 +35,10 @@ python demo/video_demo.py \
 #     --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
 #     --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_mixmorepatch.MOV --mix --save_imgs
 
-# export CUDA_VISIBLE_DEVICES=0
-# python demo/video_demo.py \
-#     configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch.py \
-#     data/td/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch/latest.pth \
-#     --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
-#     --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
-#     --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_patch.MOV --mix --save_imgs
+export CUDA_VISIBLE_DEVICES=0
+python demo/video_demo.py \
+    configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch.py \
+    data/td/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch/latest.pth \
+    --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
+    --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
+    --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_patch_mcls.MOV --mix --save_imgs
