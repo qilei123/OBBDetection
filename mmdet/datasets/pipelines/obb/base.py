b54aba277efa87c9fd2a86bb7333f0968cfb4fec
qilei123 <qileimail123@gmail.com>
2021-11-15 23:55:58 -0500

    dev

--- mmdet/datasets/pipelines/obb/base.py
+++ mmdet/datasets/pipelines/obb/base.py
@@ -394,7 +394,7 @@ class RandomOBBRotate(object):
         print("----------------before-----------------")
         print(results)
         
-        for box in results['ann']['bboxes']:
+        for box in results['ann_info']['bboxes']:
             cv2.line(results['img'], (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), thickness=1)
             cv2.line(results['img'], (int(box[2]), int(box[3])), (int(box[4]), int(box[5])), (0, 255, 0), thickness=1)
             cv2.line(results['img'], (int(box[4]), int(box[5])), (int(box[6]), int(box[7])), (0, 255, 0), thickness=1)
@@ -444,7 +444,7 @@ class RandomOBBRotate(object):
             results[k] = cv2.warpAffine(results[k], matrix, (w, h))
         print("---------------after------------------")
         print(results)
-        for box in results['ann']['bboxes']:
+        for box in results['ann_info']['bboxes']:
             cv2.line(results['img'], (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), thickness=1)
             cv2.line(results['img'], (int(box[2]), int(box[3])), (int(box[4]), int(box[5])), (0, 255, 0), thickness=1)
             cv2.line(results['img'], (int(box[4]), int(box[5])), (int(box[6]), int(box[7])), (0, 255, 0), thickness=1)
