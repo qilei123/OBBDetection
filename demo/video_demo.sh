cc01f9fa03e612123294df2d41882e0b3779def3
qilei123 <qileimail123@gmail.com>
2021-11-16 11:47:49 -0500

    dev

--- demo/video_demo.sh
+++ demo/video_demo.sh
@@ -35,10 +35,10 @@
 #     --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
 #     --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_mixmorepatch.MOV --mix --save_imgs
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 python demo/video_demo.py \
     configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch.py \
     data/td/work_dirs/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch_rotate/latest.pth \
     --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
-    --video_dir "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV" \
-    --out_dir "/home/qilei/DATASETS/trans_drone/DJI_0063_patch_rotate.MOV" --mix --save_imgs
+    --video_dir "/home/qilei/DATASETS/trans_drone/trans_drone_videos/DJI_0601 Wide View 400.MOV" \
+    --out_dir "/home/qilei/DATASETS/trans_drone/DJI_0601 Wide View 400_patch_rotate.MOV" --mix --save_imgs
