2bde33ce5f722b4dd9179dac6ebb198164aed385
qilei123 <qileimail123@gmail.com>
2021-11-16 01:02:58 -0500

    dev

--- configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch.py
+++ configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch.py
@@ -77,7 +77,7 @@ train_pipeline = [
     dict(type='Resize', img_scale=img_scale, keep_ratio=True),
     dict(type='OBBRandomFlip', h_flip_ratio=0.5, v_flip_ratio=0.5),
     #dict(type='Normalize', **img_norm_cfg),
-    dict(type='RandomOBBRotate', rotate_after_flip=True,keep_shape=False,
+    dict(type='RandomOBBRotate', rotate_after_flip=True,keep_shape=True,
          angles=(45, 90), vert_rate=0.5),
     dict(type='Pad', size_divisor=32),
     dict(type='Mask2OBB', obb_type='obb'),
