3c412d3821ec716d932794b5109d9e375aca8a32
qilei123 <qileimail123@gmail.com>
2021-11-18 12:50:42 -0500

    dev

--- mmdet/core/anchor/__init__.py
+++ mmdet/core/anchor/__init__.py
@@ -1,6 +1,6 @@
 from .anchor_generator import AnchorGenerator, LegacyAnchorGenerator
 from .builder import ANCHOR_GENERATORS, build_anchor_generator
-from .point_generator import PointGenerator
+from .point_generator import PointGenerator,MlvlPointGenerator
 from .utils import anchor_inside_flags, calc_region, images_to_levels
 
 from .obb.theta0_anchor_generator import Theta0AnchorGenerator
@@ -8,5 +8,5 @@ from .obb.theta0_anchor_generator import Theta0AnchorGenerator
 __all__ = [
     'AnchorGenerator', 'LegacyAnchorGenerator', 'anchor_inside_flags',
     'PointGenerator', 'images_to_levels', 'calc_region',
-    'build_anchor_generator', 'ANCHOR_GENERATORS'
+    'build_anchor_generator', 'ANCHOR_GENERATORS','MlvlPointGenerator'
 ]
