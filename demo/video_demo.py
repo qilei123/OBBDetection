d46c9ede1b0c6e2f644431048e358d2df9b7693e
qilei123 <qileimail123@gmail.com>
2021-11-18 18:56:28 -0500

    dev

--- demo/video_demo.py
+++ demo/video_demo.py
@@ -68,7 +68,7 @@ def main():
             nms_cfg = dict(type='BT_nms', iou_thr=0.1)
             result = inference_detector_huge_image(model,img,args.split,nms_cfg,args.mix)
 
-        #img = show_obb_result(img,*result)
+        img = show_obb_result(img,*result)
         results = filt_results(*result)
         tmer.update_with_obbox(results,frame_number)
         img = tmer.vis(img)
