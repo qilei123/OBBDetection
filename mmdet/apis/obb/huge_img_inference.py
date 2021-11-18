72aef9c709fb060cdf79f60ec5b10ecd62438da0
qilei123 <qileimail123@gmail.com>
2021-11-18 17:31:59 -0500

    dev

--- mmdet/apis/obb/huge_img_inference.py
+++ mmdet/apis/obb/huge_img_inference.py
@@ -253,6 +253,7 @@ def merge_patch_results_mcls(results, windows, nms_cfg):
     cls_result = np.concatenate(cls_result, axis=0)
     #print(len(cls_result))
     _results, inds = nms_op(cls_result, **nms_cfg_)
+    print(cls_labels)
     cls_labels = cls_labels[inds]
     #print(len(cls_result))
 
