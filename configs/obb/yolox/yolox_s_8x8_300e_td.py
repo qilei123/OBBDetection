6c4f221123367c8a60afdd64ae447d0c3cb4b8eb
qilei123 <qileimail123@gmail.com>
2021-11-30 20:17:15 -0500

    dev

--- configs/obb/yolox/yolox_s_8x8_300e_td.py
+++ configs/obb/yolox/yolox_s_8x8_300e_td.py
@@ -40,14 +40,14 @@ resume_from = None
 workflow = [('train', 1)]
 model = dict(
     type='YOLOX',
-    backbone=dict(type='CSPDarknet', deepen_factor=0.33, widen_factor=0.5),
+    backbone=dict(type='CSPDarknet', deepen_factor=1.33, widen_factor=1.25),
     neck=dict(
         type='YOLOXPAFPN',
-        in_channels=[128, 256, 512],
-        out_channels=128,
-        num_csp_blocks=1),
+        in_channels=[320, 640, 1280],
+        out_channels=320,
+        num_csp_blocks=4),
     bbox_head=dict(
-        type='YOLOXHead', num_classes=3, in_channels=128, feat_channels=128),)
+        type='YOLOXHead', num_classes=3, in_channels=320, feat_channels=320),)
 train_cfg=dict(assigner=dict(type='SimOTAAssigner', center_radius=2.5))
 test_cfg=dict(score_thr=0.01, nms=dict(type='nms', iou_threshold=0.65))
 data_root = 'data/coco/'
