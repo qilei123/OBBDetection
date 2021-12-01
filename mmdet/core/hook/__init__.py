0e0b7285a58ffca6658e5421708e8a7784f34ebf
qilei123 <qileimail123@gmail.com>
2021-11-30 19:24:56 -0500

    dev

---
+++ mmdet/core/hook/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .yolox_lrupdater_hook import YOLOXLrUpdaterHook
+from .yolox_mode_switch_hook import YOLOXModeSwitchHook
+
+__all__ = [
+    'YOLOXModeSwitchHook',
+     'YOLOXLrUpdaterHook',
+]
