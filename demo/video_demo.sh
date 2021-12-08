e6b1843826a99985a90539677a662d7488b126b6
qilei123 <qileimail123@gmail.com>
2021-12-08 18:48:24 -0500

    dev

--- demo/video_demo.sh
+++ demo/video_demo.sh
@@ -35,10 +35,26 @@
 #     --video_dir /home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV \
 #     --out_dir /home/qilei/DATASETS/trans_drone/DJI_0063_mixmorepatch.MOV --mix --save_imgs
 
+# export CUDA_VISIBLE_DEVICES=1
+# export CONFIG=fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch
+# export PARAM_FOLDER=${CONFIG}_rotate
+# export VIDEO_DIR=/home/qilei/DATASETS/trans_drone/trans_drone_videos2
+# files=$(ls $VIDEO_DIR)
+# for VIDEO_NAME in $files
+# do
+#     echo $VIDEO_NAME
+#     python demo/video_demo.py \
+#         configs/obb/app/trans_drone/$CONFIG.py \
+#         data/td/work_dirs/${PARAM_FOLDER}/latest.pth \
+#         --split BboxToolkit/tools/split_configs/trans_drone/aw_test.json \
+#         --video_dir "${VIDEO_DIR}/${VIDEO_NAME}" \
+#         --out_dir "/home/qilei/DATASETS/trans_drone/andover_worster/work_dirs/${PARAM_FOLDER}/video_results/${VIDEO_NAME}" --mix --save_imgs
+# done
+
 export CUDA_VISIBLE_DEVICES=1
 export CONFIG=fcos_obb_r50_fpn_gn-head_4x4_1x_td_patch
 export PARAM_FOLDER=${CONFIG}_rotate
-export VIDEO_DIR=/home/qilei/DATASETS/trans_drone/trans_drone_videos2
+export VIDEO_DIR=/home/qilei/DATASETS/trans_drone/trans_drone_videos
 files=$(ls $VIDEO_DIR)
 for VIDEO_NAME in $files
 do
@@ -50,5 +66,4 @@ do
         --video_dir "${VIDEO_DIR}/${VIDEO_NAME}" \
         --out_dir "/home/qilei/DATASETS/trans_drone/andover_worster/work_dirs/${PARAM_FOLDER}/video_results/${VIDEO_NAME}" --mix --save_imgs
 done
-
 
