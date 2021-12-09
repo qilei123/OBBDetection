7d8143b5100895d9481e6a56690e6618749369fa
qilei123 <qileimail123@gmail.com>
2021-12-08 19:18:07 -0500

    dev

--- demo/video_demo.sh
+++ demo/video_demo.sh
@@ -59,8 +59,8 @@ files=$(ls $VIDEO_DIR|tr " " "\?")
 echo $files
 for VIDEO_NAME in $files
 do
-    echo VIDEO_NAME=${"$VIDEO_NAME"|tr "\?" " "}
-    echo $VIDEO_NAME
+    echo VIDEO_NAME1=${"$VIDEO_NAME"|tr "\?" " "}
+    echo $VIDEO_NAME1
     #python demo/video_demo.py \
     #    configs/obb/app/trans_drone/$CONFIG.py \
     #    data/td/work_dirs/${PARAM_FOLDER}/latest.pth \
