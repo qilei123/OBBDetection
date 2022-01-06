b8a834ee72c28fdc8143b5040d10cc2424f4b584
qilei123 <qileimail123@gmail.com>
2022-01-06 02:06:39 -0500

    set roi for image

--- demo/demo_util.py
+++ demo/demo_util.py
@@ -188,6 +188,7 @@ def polygon_vs_roi(polygon,roi):
 def filt_results_with_roi(obboxes,cls_labels,score_thr = 0.3,roi = []):
     results = []
     for obbox, cls_label in zip(obboxes,cls_labels):
+        print(obbox)
         polygon = det2polygon(obbox[:-1])
         if obbox[-1]>score_thr and polygon_vs_roi(polygon,roi):
             results.append(np.append(obb2obbox(obbox[:-1]),[obbox[-1],cls_label]))
