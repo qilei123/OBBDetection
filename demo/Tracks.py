af7bedc2ffed6c4bcc4d5bc6527bee0809f548d1
qilei123 <qileimail123@gmail.com>
2022-01-13 20:35:09 -0500

    test

--- demo/Tracks.py
+++ demo/Tracks.py
@@ -131,10 +131,11 @@ class tracks_manager:
             for live_id in self.live_ids:
                 latest_track = self.track_queue[live_id][-1]
                 p2 = self.det2polygon(latest_track.polygon)
-                print(p2.area)
                 print(p1.area)
+                print(p2.area)
                 print(p1.intersection(p2).area)
-                iou_area = p1.intersection(p2).area
+                intersect_area = p1.intersection(p2).area
+                union_area = p1.union(p2).area
                 if iou_area>max_iou:
                     max_iou = iou_area
                     max_iou_id = live_id
