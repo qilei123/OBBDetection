434f028288f81744eeee13cdb55d4df955bfabde
qilei123 <qileimail123@gmail.com>
2022-01-29 00:58:22 -0500

    test

--- demo/demo_util.py
+++ demo/demo_util.py
@@ -305,6 +305,7 @@ def init_trackers(frame,hbb_results):
         bbox = (hbb_result[0],hbb_result[1],hbb_result[2],hbb_result[3])
         tracker.init(frame,bbox)
         trackers.append(tracker)
+        print(hbb_result)
         other_infos.append[[hbb_result[-2],hbb_result[-1]]]
     return trackers,other_infos
 
