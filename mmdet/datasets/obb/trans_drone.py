4322db4ef66031360183ae4db4f344369f77d8e0
qilei123 <qileimail123@gmail.com>
2022-03-18 02:33:51 -0400

    test

--- mmdet/datasets/obb/trans_drone.py
+++ mmdet/datasets/obb/trans_drone.py
@@ -123,6 +123,7 @@ class TDDataset(CustomDataset):
         valid_inds = []
         if self.coco_type:
             ids_with_ann = set(_['image_id'] for _ in self.coco.anns.values())
+            print(self.data_infos)
             for i, img_info in enumerate(self.data_infos):
                 if self.filter_empty_gt and self.img_ids[i] not in ids_with_ann:
                     continue
