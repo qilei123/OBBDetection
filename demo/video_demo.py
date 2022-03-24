19a59b23abfc14922ce5e0749f4050744fc05428
qilei123 <qileimail123@gmail.com>
2022-03-24 16:54:46 -0400

    test

--- demo/video_demo.py
+++ demo/video_demo.py
@@ -79,6 +79,7 @@ def main():
         if not args.split:
             result = inference_detector(model, img)
             for cat_id,re in enumerate( result):
+                print(re)
                 cat_ids = np.ones(len(re))*cat_id
                 img = show_obb_result(img,re,cat_ids)
         else:
