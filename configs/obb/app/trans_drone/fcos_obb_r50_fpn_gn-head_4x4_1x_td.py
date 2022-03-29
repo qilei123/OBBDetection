562c089df6ac25b16903c4b1d33e44ada4b737ec
qilei123 <qileimail123@gmail.com>
2022-03-29 17:22:46 -0400

    test

--- configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td.py
+++ configs/obb/app/trans_drone/fcos_obb_r50_fpn_gn-head_4x4_1x_td.py
@@ -110,8 +110,8 @@ test_pipeline = [
 # does evaluation while training
 # uncomments it  when you need evaluate every epoch
 data = dict(
-    samples_per_gpu=4,
-    workers_per_gpu=2,
+    samples_per_gpu=2,
+    workers_per_gpu=1,
     train=dict(
         ann_file=data_root + 'annotations/train_AW_C5.json',
         img_prefix=data_root + 'images/',
